92f0cc032a81ce0c5709957d9ab519eda416c660
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-10-13 20:41:33 +0100

    refactor repository class tests and methods

--- src/main/java/Repository.java
+++ src/main/java/Repository.java
@@ -5,6 +5,8 @@ public class Repository {
     private String name;
     private String description;
     private RepositoryType repositoryType;
+
+//    give repository an ArrayList "commits" that hold instances of the Commit objects
     private ArrayList<Commit> commits;
 
     public Repository(String name, String description, RepositoryType repositoryType){
@@ -24,5 +26,42 @@ public class Repository {
 
     public RepositoryType getType() {
         return this.repositoryType;
+    }
+//  as Commit is a class with it's own properties so we have to pass in the entire object
+    public void addCommit(Commit commit) {
+        this.commits.add(commit);
+    }
+
+    public int countCommits() {
+        return this.commits.size();
+    }
+
+    public Commit getCommitById(int uniqueId) {
+        Commit foundCommit = null;
+//        need to loop through the commits
+        for (Commit commit : this.commits){
+//            if the commit id = unique id you pass through add it to variable foundCommit
+            if (commit.getId() == uniqueId){
+                foundCommit = commit;
+            }
+        }return foundCommit;
+    }
+
+    public String getCommitDescriptionById(int uniqueId){
+        String foundCommit = null;
+        for (Commit commit : this.commits){
+            if (commit.getId() == uniqueId){
+                foundCommit = commit.getDescription();
+            }
+        }return foundCommit;
+    }
+
+    public Commit getCommitByType(CommitType feature) {
+        Commit foundCommit = null;
+        for (Commit commit : this.commits){
+            if (commit.getCommitType() == feature){
+                foundCommit = commit;
+            }
+        }return foundCommit;
     }
 }
--- src/test/java/RepositoryTest.java
+++ src/test/java/RepositoryTest.java
@@ -28,4 +28,28 @@ public class RepositoryTest {
     public void repositoryHasType(){
         assertEquals(RepositoryType.PUBLIC, repo.getType());
     }
+
+    @Test
+    public void canAddCommit(){
+        repo.addCommit(commit);
+        assertEquals(1, repo.countCommits());
+    }
+
+    @Test
+    public void canGetCommitById(){
+        repo.addCommit(commit);
+        assertEquals(commit, repo.getCommitById(1));
+    }
+
+    @Test
+    public void canGetCommitDescriptionById(){
+        repo.addCommit(commit);
+        assertEquals(commit.getDescription(), repo.getCommitDescriptionById(1));
+    }
+
+    @Test
+    public void canGetCommitByType(){
+        repo.addCommit(commit);
+        assertEquals(commit, repo.getCommitByType(CommitType.FEATURE));
+    }
 }
